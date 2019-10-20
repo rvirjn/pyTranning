16e3a8ba5bc79648909ca8312d18deda914ece1a
Ravi Ranjan <raviranjan.amu@gmail.com>
2019-10-20 21:51:47 +0530

    Update vrops-test.py

--- vrops-test.py
+++ vrops-test.py
@@ -2,5 +2,5 @@ from nagini import Nagini
 
 if __name__ == "__main__":
     print("Connecting to vROps")
-    vcops = Nagini(host="vrops01svr01.rainpole.local", user_pass=("admin", "VMware1!"))
+    vcops = Nagini(host="", user_pass=("admin", ""))
     print("Completed")
